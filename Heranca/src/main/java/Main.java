3c79fc39e3a870cb16152251f50bdd252d69b8c5
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2022-02-07 00:50:12 -0300

    Exemplo herança e polimorfismo

---
+++ Heranca/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Gerente gerente = new Gerente();
+       Analista analista = new Analista();
+
+
+        System.out.println("Bonoficação Analista " + analista.bonificacao(1000d));
+        System.out.println("Bonoficação Gerente " + gerente.bonificacao(1000d));
+
+    }
+}
